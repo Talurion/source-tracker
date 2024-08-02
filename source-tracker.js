59d8c74bebd066000dc85669206afe6e7b250a24
Anton Yushkov <a.s.yushkov@bk.ru>
2024-08-02 22:31:26 +0300

    selectOrganicSource

--- source-tracker.js
+++ source-tracker.js
@@ -41,30 +41,48 @@ function isNotEmpty(value) {
 }
 
 function isInternalTransition(merge) {
-    if (!document.referrer) {
-        console.log('Это не внутренний переход');
+    if (!document.referrer) { // проверяем есть ли referrer, если нет возвращаем false
         return false;
     }
 
     const currentUrlObject = getUrlObject(document.URL);
     const referrerUrlObject = getUrlObject(document.referrer);
 
-    if (merge) {
-        console.log('Это внутренний переход? ' + currentUrlObject.mainDomain === referrerUrlObject.mainDomain);
-        return currentUrlObject.mainDomain === referrerUrlObject.mainDomain;
-    }
-    console.log('Это внутренний переход? ' + currentUrlObject.domain === referrerUrlObject.domain);
-    return currentUrlObject.domain === referrerUrlObject.domain;
+    if (merge) { // если домены и поддомены отслеживаем вместе
+        return currentUrlObject.mainDomain === referrerUrlObject.mainDomain; // сравниваем домен реферера и домен текущей страницы
+    } // если домены и поддомены отслеживаем отдельно
+    return currentUrlObject.domain === referrerUrlObject.domain; // сравниваем hostname реферера и hostname текущей страницы
 }
 
 function hasUTMParameters() {
     const currentUrlObject = getUrlObject(document.URL);
 
-    const hasUtmSource = isNotEmpty(currentUrlObject.utmSource);
+    const hasUtmSource = isNotEmpty(currentUrlObject.utmSource); // проверяем есть ли что то в переменных UTM
     const hasUtmMedium = isNotEmpty(currentUrlObject.utmMedium);
     const hasUtmCampaign = isNotEmpty(currentUrlObject.utmCampaign);
 
     return hasUtmSource || hasUtmMedium || hasUtmCampaign;
+}
+
+function selectOrganicSource (mainDomain) {
+    let organicSource = '';
+    switch (mainDomain) {
+        case 'ya.ru': organicSource = 'yandex'; break;
+        case 'yandex.ru': organicSource = 'yandex'; break;
+        case 'google.com': organicSource = 'google'; break;
+        case 'bing.com': organicSource = 'bing'; break;
+        case 'go.mail.ru': organicSource = 'mail.ru'; break;
+        case 'yahoo.com': organicSource = 'yahoo'; break;
+        case 'rambler.ru': organicSource = 'rambler'; break;
+        case 'duckduckgo.com': organicSource = 'duckduckgo'; break;
+        case 'about.com': organicSource = 'about.com'; break;
+        case 'aol.com': organicSource = 'aol.com'; break;
+        case 'ask.com': organicSource = 'ask.com'; break;
+        case 'globososo.com': organicSource = 'globososo'; break;
+        case 'tut.by': organicSource = 'tut.by'; break;
+        default:  organicSource = 'notFound'; break;
+    }
+    return organicSource;
 }
 
 console.log(getUrlObject(document.URL));
